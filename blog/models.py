d94b29ef736ef1184b9692c1a895786f3329d7ff
Feedor <zfed2004@mail.ru>
2019-06-02 20:34:15 +0700

    My first commit

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+class Post(models.Model):
+    #models.ForeignKey - ссылка на другую модель 
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #models.CharField —  текстовое поле с ограничением на количество символов.
+    title = models.CharField(max_length=200)
+    #models.TextField - поле для неограничено длинного текста
+    text = models.TextField()
+    #models.DateTimeField- дата и время
+    create_date = models.DateTimeField(default=timezone.now)
+    pulikate_date = models.DateTimeField(blank=True, null=True)
+    def publ(self):
+        self.pulikate_date = timezone.now()
+        self.save()
